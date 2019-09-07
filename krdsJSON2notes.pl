e6b6a96d0f49a1dbb34a3dfb982bbbee2b3aebdb
jps-e <jps-e@users.noreply.github.com>
2019-09-07 14:25:04 -0700

    Add option to extract highlighted text from rawml
    This will only work with the assembled_text.dat produced by
    kindleunpack -d

--- krdsJSON2notes.pl
+++ krdsJSON2notes.pl
@@ -1,9 +1,19 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Getopt::Long;
 use JSON;
 
-open FH, "<", shift @ARGV or die "Can't open JSON input file";
+my ($inp, $buf, $loc, $n, $hl, $raw); my $highlight=0; my $note=0;
+GetOptions(
+           "inp=s" => \$inp,
+           "h"     => \$highlight,
+           "n"     => \$note,
+           "raw=s" => \$raw
+          );
+if ($raw) { open FHraw, $raw or die "Can't open $raw for reading"; }
+$note = 1 unless $highlight or $note;
+open(FH, "<", $inp) or die "Can't open JSON input file: '$inp'";
 my $json = do { local $/; <FH> };
 close FH;
 my %krds = %{decode_json($json)};
@@ -11,14 +21,23 @@ my @names = keys %krds;
 for (@names) {
   next unless /annotation.cache.object/;
   my %annotations = %{$krds{$_}};
-  my @notes = $annotations{'annotation.personal.note'};
-  @notes = @{$notes[0]};
-  for my $n (@notes) {
-    print "$n->{startPosition}\t$n->{endPosition}\tNote:\t'$n->{note}'\n";
+  if ($highlight) {
+    my @notes = $annotations{'annotation.personal.highlight'};
+    @notes = @{$notes[0]};
+    for my $n (@notes) {
+      if ($raw) {
+        seek FHraw, $n->{startPosition}, 0;
+        read FHraw, $hl, $n->{endPosition} - $n->{startPosition} + 1;
+        $hl = "\t'$hl'";
+      } else { $hl = ''; }
+      print "$n->{startPosition}\t$n->{endPosition}\tHighlight:$hl\n";
+    }
   }
-  @notes = $annotations{'annotation.personal.highlight'};
-  @notes = @{$notes[0]};
-  for my $n (@notes) {
-    print "$n->{startPosition}\t$n->{endPosition}\tHighlight:\n";
+  if ($note) {
+    my @notes = $annotations{'annotation.personal.note'};
+    @notes = @{$notes[0]};
+    for my $n (@notes) {
+      print "$n->{startPosition}\t$n->{endPosition}\tNote:\t'$n->{note}'\n";
+    }
   }
 }
