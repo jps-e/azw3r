a29aee5aa7aba6c77f776b9f5671b6894a291011
jps-e <jps-e@users.noreply.github.com>
2019-09-07 10:59:46 -0700

    Add krdsJSON2notes.pl
    Parse JSON from krds.py into form compatible with azw3r.pl output.

---
+++ krdsJSON2notes.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use JSON;
+
+open FH, "<", shift @ARGV or die "Can't open JSON input file";
+my $json = do { local $/; <FH> };
+close FH;
+my %krds = %{decode_json($json)};
+my @names = keys %krds;
+my @vals = values %krds;
+for (@names) {
+  next unless /annotation.cache.object/;
+  my %annotations = %{$krds{$_}};
+  my @notes = $annotations{'annotation.personal.note'};
+  @notes = @{$notes[0]};
+  for my $n (@notes) {
+    print "$n->{startPosition}\t$n->{endPosition}\tNote:\t'$n->{note}'\n";
+  }
+}
